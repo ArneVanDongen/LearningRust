2a2f431757f74141fc833cfbfda0da220942fede
ArneVanDongen <arnevdongen@gmail.com>
2021-05-18 19:35:46 +0200

    Used sdl2 demo and adapted it with returning a Result

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,51 @@
-#[derive(Debug)]
-struct Position {
-    x: i8,
-    y: i8,
-}
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
+ 
+pub fn main() -> Result<(), String> {
+    let sdl_result = sdl2::init();
 
-impl Position {
-    fn update_x(&mut self) {
-        self.x += 1;
-        self.update_y();
+    let sdl_context = match sdl_result {
+        Ok(result) => result,
+        Err(message) => {
+            println!("SDL reported an error: '{}'", message);
+            return Ok(());
+        }
+    };
+
+    let video_subsystem = sdl_context.video()?;
+ 
+    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+ 
+    let mut canvas = window.into_canvas().build().unwrap();
+ 
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
-    fn update_y(&mut self) {
-        self.y -= 10;
-    }
-}
-
-fn main() {
-    let mut pos = Position { x: 42, y: 13 };
-    println!("Created position: {:?}", pos);
-    pos.update_x();
-    println!("updated position: {:?}", pos);
-    Position::update_y(&mut pos);
-    println!("updated position: {:?}", pos);
-
+    
+    Ok(())
 }
