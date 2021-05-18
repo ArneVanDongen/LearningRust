03e782acb7b3d73669d0fd4e20dda3a69379c948
ArneVanDongen <arnevdongen@gmail.com>
2021-05-18 16:55:29 +0200

    Added position struct, implemented some methods to play around with borrowing etc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#[derive(Debug)]
+struct Position {
+    x: i8,
+    y: i8,
+}
+
+impl Position {
+    fn update_x(&mut self) {
+        self.x += 1;
+        self.update_y();
+    }
+    fn update_y(&mut self) {
+        self.y -= 10;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut pos = Position { x: 42, y: 13 };
+    println!("Created position: {:?}", pos);
+    pos.update_x();
+    println!("updated position: {:?}", pos);
+    Position::update_y(&mut pos);
+    println!("updated position: {:?}", pos);
+
 }
