096dc9227490ffda012954e7e078d564a5b6ccf6
ArneVanDongen <Arne.vdongen@gmail.com>
2021-06-01 16:16:50 +0200

    completed traits exercises

--- rustlings/exercises/traits/traits2.rs
+++ rustlings/exercises/traits/traits2.rs
@@ -10,13 +10,18 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 //TODO: Add your code here
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut owned_vec = self.to_owned();
+        owned_vec.push(String::from("Bar"));
+        owned_vec
+    }
+}
 
 #[cfg(test)]
 mod tests {
