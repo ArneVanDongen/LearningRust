50d31a9d5173996e33bb24de92a944a7aa0df101
ArneVanDongen <Arne.vdongen@gmail.com>
2021-06-03 12:20:47 +0200

    completed iterator exercises

--- rustlings/exercises/standard_library_types/iterators4.rs
+++ rustlings/exercises/standard_library_types/iterators4.rs
@@ -1,7 +1,5 @@
 // iterators4.rs
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -12,6 +10,16 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    let iter = 0..num;
+
+    let mut fact = num;
+    for item in iter.rev() {
+        fact = match item {
+            0 => fact,
+            _ => fact * item
+        }
+    }
+    fact
 }
 
 #[cfg(test)]
