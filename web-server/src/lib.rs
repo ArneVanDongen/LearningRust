6ab2dab57861449385a4b94e542ed0a70722b57b
ArneVanDongen <Arne.vdongen@gmail.com>
2021-06-10 14:53:50 +0200

    Changed from join handles to a pool of workers, with a channel going from one threadpool to many workers.

--- web-server/src/lib.rs
+++ web-server/src/lib.rs
@@ -1,32 +1,54 @@
+use std::sync::{Arc, Mutex, mpsc};
 use std::thread;
-
-pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
-}
 
 #[derive(Debug)]
 pub enum PoolCreationError {
     SizeOfZero,
 }
 
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+struct Job;
+
 impl ThreadPool {
     pub fn new(size: usize) -> Result<ThreadPool, PoolCreationError> {
         match size {
             0 => Err(PoolCreationError::SizeOfZero),
             _ => {
-                let mut threads = Vec::with_capacity(size);
-                for _ in 0..size {
-                    // create some threads and store them in the vector
+                let (sender, receiver) = mpsc::channel();
+                let receiver = Arc::new(Mutex::new(receiver));
+                let mut workers = Vec::with_capacity(size);
+
+                for id in 0..size {
+                    workers.push(Worker::new(id, Arc::clone(&receiver)));
                 }
-        
-                Ok(ThreadPool { threads })
-            },        
+
+                Ok(ThreadPool { workers, sender })
+            }
         }
     }
-    
+
     pub fn execute<F>(&self, f: F)
     where
-        F: FnOnce() + Send + 'static {
+        F: FnOnce() + Send + 'static,
+    {
+    }
+}
 
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
+
+        Worker { id, thread }
     }
 }
