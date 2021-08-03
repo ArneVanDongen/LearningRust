699e42486bdb740f7ac83c001349bc9f77d92460
ArneVanDongen <Arne.vdongen@gmail.com>
2021-08-03 16:23:14 +0200

    Created WebGL setup, and saving it in our Client

--- graphics_in_the_browser/src/lib.rs
+++ graphics_in_the_browser/src/lib.rs
@@ -1,23 +1,31 @@
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
+use web_sys::WebGlRenderingContext as GL;
+
+mod gl_setup;
 
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn error(s: &str);
 }
 
 #[wasm_bindgen]
 pub struct Client {
-
+    gl: GL,
 }
 
 #[wasm_bindgen]
 impl Client {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
-        Self {
+        console_error_panic_hook::set_once();
+        let gl = gl_setup::initialize_webgl_context().unwrap();
 
+        Self {
+            gl
         }
     }
 
@@ -26,6 +34,6 @@ impl Client {
     }
 
     pub fn render(& self) {
-
+        self.gl.clear(GL::COLOR_BUFFER_BIT | GL::DEPTH_BUFFER_BIT);
     }
 }
